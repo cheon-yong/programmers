dcc4fda7520e4460be0b14c97475c91a19a23705
yongjun <yes1312@naver.com>
2021-06-06 00:46:44 +0900

    test

--- 스택큐/다리를지나는트럭.cpp
+++ 스택큐/다리를지나는트럭.cpp
@@ -1,9 +1,38 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
+    int next = 0;
+    // 무게 시간
+    vector<pair<int, int>> bridge;
+    while (!bridge.empty() || !truck_weights.empty())
+    {
+        answer++;
+        int bridgeWeight = 0;
+        for (int i = 0; i < bridge.size(); i++)
+        {
+            bridge[i].second++;
+            if (bridge[i].second > bridge_length)
+            {
+                bridge.erase(bridge.begin() + i);
+                continue;
+            }
+            bridgeWeight += bridge[i].first;
+        }
+        if (weight > bridgeWeight)
+        {
+            if (weight >= bridgeWeight + truck_weights[next])
+            {
+                bridge.push_back(make_pair(truck_weights[next], 1));
+                truck_weights.erase(truck_weights.begin() + next);
+                next++;
+            }
+        }
+    }
     return answer;
 }
